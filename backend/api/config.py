62c50f84290de12702bd883962ef6c168c434d84
abyt101 <aberhamyirsaw@gmail.com>
2024-07-03 20:39:51 +0300

    Readme updated

--- backend/api/config.py
+++ backend/api/config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://sa:123456@DESKTOP-AFEFNC1/RAG?driver=ODBC+Driver+17+for+SQL+Server'
+    SQLALCHEMY_DATABASE_URI = 'connection_string'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'your_secret_key'
+    JWT_SECRET_KEY = 'secret_key'
